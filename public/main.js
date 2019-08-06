861614719ba413e1eb95c16243d084a029544fa1
Tony Grimm <anthony.l.grimm@gmail.com>
2019-08-06 16:34:20 -0400

    final pass, explorer mode

--- public/main.js
+++ public/main.js
@@ -2,80 +2,137 @@ const main = () => {
   console.log('Hello, World!')
 }
 
+let team1ScoreCounter = 0
+let team2ScoreCounter = 0
+
 const updateTeam1Name = () => {
   console.log('updated value in text box')
   // Get the team name from the input tag store it as variable
-  let team1Name = document.querySelector('.team-1-input').value
+  const team1Name = document.querySelector('.team-1-input').value
   console.log(team1Name)
 
   // Update the html with new name
   document.querySelector('.team-1-name').textContent = team1Name
 }
 
-const add1Team1Score = () => {
-  console.log('updated value in text box')
-  // Get the current score and store it as variable
-  let team1Score = document.querySelector('.team-1-score').textContent
-  console.log(team1Score)
-
-  // Calculate the add 1
-  const updateTeam1Score = parseFloat(team1Score) + 1
-  console.log(updateTeam1Score)
-
-  // Update the html with new score
-  document.querySelector('.team-1-score').textContent = updateTeam1Score
-}
-
-const subtract1Team1Score = () => {
-  console.log('updated value in text box')
-  // Get the current score and store it as variable
-  let team1Score = document.querySelector('.team-1-score').textContent
-  console.log(team1Score)
-
-  // Calculate the subtract 1
-  const updateTeam1Score = parseFloat(team1Score) - 1
-  console.log(updateTeam1Score)
-
-  // Update the html with new score
-  document.querySelector('.team-1-score').textContent = updateTeam1Score
-}
-
 const updateTeam2Name = () => {
   console.log('updated value in text box')
   // Get the team name from the input tag store it as variable
-  let team2Name = document.querySelector('.team-2-input').value
+  const team2Name = document.querySelector('.team-2-input').value
   console.log(team2Name)
 
   // Update the html with new name
   document.querySelector('.team-2-name').textContent = team2Name
 }
 
-const add1Team2Score = () => {
-  console.log('updated value in text box')
-  // Get the current score and store it as variable
-  let team2Score = document.querySelector('.team-2-score').textContent
-  console.log(team2Score)
-
-  // Calculate the add 1
-  const updateTeam2Score = parseFloat(team2Score) + 1
-  console.log(updateTeam2Score)
-
-  // Update the html with new score
-  document.querySelector('.team-2-score').textContent = updateTeam2Score
+// Check team 1 score less than or = 21 add 1 to score
+const add1Team1Score = () => {
+  // check the team score
+  const team1Score = document.querySelector('.team-1-score')
+  console.log(team1Score.textContent)
+  if (team1Score.textContent < 20) {
+    // add 1 to team 1 score
+    console.log('updated value in text box')
+    // Get the current score and store it as variable
+    team1ScoreCounter += 1
+    team1Score.textContent = team1ScoreCounter
+    console.log(team1Score.textContent)
+  } else {
+    team1Score.textContent = 'You Win!'
+    team1Score.classList.add('winning-team')
+    document.querySelector('.team-1-add-1-button').classList.add('hide')
+    document.querySelector('.team-1-subtract-1-button').classList.add('hide')
+    document.querySelector('.team-2-add-1-button').classList.add('hide')
+    document.querySelector('.team-2-subtract-1-button').classList.add('hide')
+    document.querySelector('.reset-button').classList.remove('hide')
+    console.log(team1Score)
+  }
 }
 
+// Check team 1 score greater than 0 and then subtract 1
+const subtract1Team1Score = () => {
+  // check the team score
+  const team1Score = document.querySelector('.team-1-score')
+  console.log(team1Score.textContent)
+  if (team1Score.textContent > 1) {
+    // subtract 1 from team 1 score
+    console.log('updated value in text box')
+    // Get the current score and store it as variable
+    team1ScoreCounter -= 1
+    team1Score.textContent = team1ScoreCounter
+    console.log(team1Score)
+  } else {
+    team1Score.textContent = 0
+    console.log(team1Score)
+  }
+}
+
+// Check team 2 score less than or = 21 add 1 to score
+const add1Team2Score = () => {
+  // check the team score
+  const team2Score = document.querySelector('.team-2-score')
+  console.log(team2Score.textContent)
+  if (team2Score.textContent < 20) {
+    // add 1 to team 2 score
+    console.log('updated value in text box')
+    // Get the current score and store it as variable
+    team2ScoreCounter += 1
+    team2Score.textContent = team2ScoreCounter
+    console.log(team2Score.textContent)
+  } else {
+    team2Score.textContent = 'You Win!'
+    team2Score.classList.add('winning-team')
+    document.querySelector('.team-1-add-1-button').classList.add('hide')
+    document.querySelector('.team-1-subtract-1-button').classList.add('hide')
+    document.querySelector('.team-2-add-1-button').classList.add('hide')
+    document.querySelector('.team-2-subtract-1-button').classList.add('hide')
+    document.querySelector('.reset-button').classList.remove('hide')
+    console.log(team2Score)
+  }
+}
+
+// Check team 2 score greater than 0 and then subtract 1
 const subtract1Team2Score = () => {
-  console.log('updated value in text box')
-  // Get the current score and store it as variable
-  let team2Score = document.querySelector('.team-2-score').textContent
+  // check the team score
+  const team2Score = document.querySelector('.team-2-score')
+  console.log(team2Score.textContent)
+  if (team2Score.textContent > 1) {
+    // subtract 1 from team 2 score
+    console.log('updated value in text box')
+    // Get the current score and store it as variable
+    team2ScoreCounter -= 1
+    team2Score.textContent = team2ScoreCounter
+    console.log(team2Score)
+  } else {
+    team2Score.textContent = 0
+    console.log(team2Score)
+  }
+}
+
+const resetGame = () => {
+  // Clear team 1 score
+  const team1Score = document.querySelector('.team-1-score')
+  console.log(team1Score.textContent)
+  team1ScoreCounter = 0
+  team1Score.textContent = team1ScoreCounter
+  document.querySelector('.team-1-add-1-button').classList.remove('hide')
+  document.querySelector('.team-1-subtract-1-button').classList.remove('hide')
+  document.querySelector('.team-2-add-1-button').classList.remove('hide')
+  document.querySelector('.team-2-subtract-1-button').classList.remove('hide')
+  document.querySelector('.reset-button').classList.add('hide')
+  console.log(team1Score)
+
+  // Clear team 2 score
+  const team2Score = document.querySelector('.team-2-score')
+  console.log(team2Score.textContent)
+  team2ScoreCounter = 0
+  team2Score.textContent = team2ScoreCounter
+  document.querySelector('.team-1-add-1-button').classList.remove('hide')
+  document.querySelector('.team-1-subtract-1-button').classList.remove('hide')
+  document.querySelector('.team-2-add-1-button').classList.remove('hide')
+  document.querySelector('.team-2-subtract-1-button').classList.remove('hide')
+  document.querySelector('.reset-button').classList.add('hide')
   console.log(team2Score)
-
-  // Calculate the subtract 1
-  const updateTeam2Score = parseFloat(team2Score) - 1
-  console.log(updateTeam2Score)
-
-  // Update the html with new score
-  document.querySelector('.team-2-score').textContent = updateTeam2Score
 }
 
 document.addEventListener('DOMContentLoaded', main)
@@ -115,3 +172,6 @@ document.querySelector('.team-2-score')
 document
   .querySelector('.team-2-subtract-1-button')
   .addEventListener('click', subtract1Team2Score)
+
+// Reset Game
+document.querySelector('.reset-button').addEventListener('click', resetGame)
